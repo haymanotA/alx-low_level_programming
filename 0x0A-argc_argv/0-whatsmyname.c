9a81599d684426a9abd12a11af1f13356f2fb172
haymanotA <haymedu21@gmail.com>
2023-07-07 16:31:51 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints name of the program
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * @argc: argument count
- * @argv: argument vector
- * Return: int
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
+
 	return (0);
 }
+
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,10 @@
+main.h
+
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
 int _putchar(char c);
-unsigned int coin_count(unsigned int count, int num);
+int _atoi(char *s);
 
-#endif /* MAIN_H */
+#endif
+
